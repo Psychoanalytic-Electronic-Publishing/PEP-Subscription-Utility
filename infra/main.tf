6f14367e8394dd40b556d44dff13991bc5dd9571
Jordan Allen <jordan@jordanallen.org>
2023-01-17 22:29:50 +0000

    Configure S3 trigger for lambda
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/main.tf
+++ infra/main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "pep_subscription_updates" {
 module "subscription_utility_lambda" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "pep-subscription-mailer-utility-app"
+  function_name = "pep-subscription-mailer-utility-${var.env}"
   source_path   = "../app"
   handler       = "subscription_service.handler"
   runtime       = "python3.8"
@@ -32,4 +32,23 @@ module "subscription_utility_lambda" {
     "TEST"   = "test-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     "TEEST2" = "test2-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
+}
+
+
+resource "aws_s3_bucket_notification" "pep_subscription_notification" {
+  bucket = aws_s3_bucket.pep_subscription_updates.id
+
+  lambda_function {
+    lambda_function_arn = module.subscription_utility_lambda.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+
+resource "aws_lambda_permission" "allow_bucket_invoke" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = module.subscription_utility_lambda.lambda_function_arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.pep_subscription_updates.arn
 }
