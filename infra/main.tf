d6a6c92e0f65540fff421b51215fb7f0e2c7b17c
Jordan Allen <jordan@jordanallen.org>
2023-01-17 21:25:54 +0000

    Use terraform-aws-modules to cut down on boilerplate
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/main.tf
+++ infra/main.tf
@@ -13,42 +13,16 @@ provider "aws" {
   region = var.aws_region
 }
 
+module "subscription_utility_lambda" {
+  source = "terraform-aws-modules/lambda/aws"
 
-provider "archive" {}
-data "archive_file" "zip" {
-  type        = "zip"
-  source_dir  = "../app"
-  output_path = "../app.zip"
-}
+  function_name = "pep-subscription-mailer-utility-app"
+  source_path   = "../app"
+  handler       = "subscription_service.handler"
+  runtime       = "python3.8"
 
-data "aws_iam_policy_document" "policy" {
-  statement {
-    sid    = ""
-    effect = "Allow"
-    principals {
-      identifiers = ["lambda.amazonaws.com"]
-      type        = "Service"
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.policy.json
-}
-
-resource "aws_lambda_function" "lambda" {
-  function_name    = "pep-subscription-mailer-utility-app"
-  filename         = data.archive_file.zip.output_path
-  source_code_hash = data.archive_file.zip.output_base64sha256
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "subscription_service.handler"
-  runtime          = "python3.8"
-  environment {
-    variables = {
-      "TEST"   = "test-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-      "TEEST2" = "test2-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    }
+  environment_variables = {
+    "TEST"   = "test-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "TEEST2" = "test2-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 }
